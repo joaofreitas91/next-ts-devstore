2097e1ceb8f7f65c2245991e133be7d667c9089b
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-10-29 17:16:44 -0300

    style: header

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
+    <div className="mx-auto grid w-full grid-rows-app min-h-screen p-8 max-w-[1600px] gap-5">
       <Header />
       {children}
     </div>
