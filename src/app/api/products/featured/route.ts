920531f4538ae669790298ec937ecc37b684af3f
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-10-29 20:48:31 -0300

    style: home skeleton screen

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,8 @@
 import data from '../data.json'
 
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1500)) // simulate network delay
+
   const featured = data.products.filter((product) => product.featured)
   return Response.json(featured)
 }
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,7 @@
 import data from './data.json'
 
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1500)) // simulate network delay
+
   return Response.json(data.products)
 }
