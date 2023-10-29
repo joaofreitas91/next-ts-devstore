13f5034559d6aad43157679f5ffaa04bef928d47
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-10-29 18:34:44 -0300

    feat: Fetch API wrapper

---
+++ src/data/api.ts
@@ -0,0 +1,11 @@
+import { env } from '@/env'
+
+export async function api(path: string, init?: RequestInit) {
+  const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
+  const prefix = '/api'
+  const url = new URL(prefix.concat(path), baseUrl)
+
+  console.log('API request:', url)
+
+  return fetch(url, init)
+}
